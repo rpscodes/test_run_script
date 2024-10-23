3b179b304cc86f1fcb1b2453e079eaf0885a253b
Vamsi Ravula <83864467+rpscodes@users.noreply.github.com>
2024-10-23 16:27:18 -0700

    Update os_script.sh

--- os_script.sh
+++ os_script.sh
@@ -4,4 +4,4 @@ curl https://skupper.io/install.sh | sh -s
 current_date=$(date +%Y-%m-%d)
 
 # Log the execution to Google Sheets via Google Form
-curl -X POST -L -d "entry.269509727=$current_date&entry.1696434522=1" "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfgxxqWZgpWOHbIp-0cGXId-1ZoTgI2zVVeMEJdLkYn6xZGkw/formResponse" > /dev/null
+curl -s -X POST -L -d "entry.269509727=$current_date&entry.1696434522=1" "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfgxxqWZgpWOHbIp-0cGXId-1ZoTgI2zVVeMEJdLkYn6xZGkw/formResponse"
